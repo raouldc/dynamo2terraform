f2b8e95465bd1e6442976cf629fabc1fc294fb5a
Raoul D'Cunha <raoul.dcunha@pushpay.com>
2018-04-06 15:01:50 +1200

    Cleanup command line options

--- dynamo2terraform/Program.cs
+++ dynamo2terraform/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using dynamo2terraform.Services;
 using Fluid;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace dynamo2terraform
 {
@@ -10,25 +11,51 @@ namespace dynamo2terraform
 	{
 		private static void Main(string[] args)
 		{
-			var file =
-				"../dynamo2terraform/TestFiles/Models/Person.cs";
-			var tree = ClassLoader.GetSyntaxTreeFromPath(file);
-			var table = DynamoParserService.Parse(tree);
+			var app = new CommandLineApplication {
+				Name = "dynamo2terraform"
+			};
+			app.HelpOption("-?|-h|--help");
 
-			var sourcePath = "../dynamo2terraform/Templates/terraform.liquid";
-			var source = File.ReadAllText(sourcePath);
+			var inputFilePathOption = app.Option("-i|--input <path>",
+				"The path to the input C# DynamoDB Model decorated with DynamoDBAttributes for parsing",
+				CommandOptionType.SingleValue);
+
+			var templateFilePathOption = app.Option("-t|--template <path>",
+				"The path to the liquid template to be used for generating the output",
+				CommandOptionType.SingleValue);
+
+			app.OnExecute(() => {
+				if (!inputFilePathOption.HasValue() || !File.Exists(inputFilePathOption.Value())) {
+					Console.WriteLine("Could not find Input file at the path provided");
+					return 0;
+				}
+
+				if (!templateFilePathOption.HasValue() || !File.Exists(templateFilePathOption.Value())) {
+					Console.WriteLine("Could not find Liquid template file at path provided");
+					return 0;
+				}
+
+				var tree = ClassLoader.GetSyntaxTreeFromPath(inputFilePathOption.Value());
+				var table = DynamoParserService.Parse(tree);
+
+				var liquidTemplate = File.ReadAllText(templateFilePathOption.Value());
 
 
-			IEnumerable<string> errors;
-			if (FluidTemplate.TryParse(source, out var template, out errors)) {
-				var context = new TemplateContext();
-				context.MemberAccessStrategy.Register(typeof(DynamoDbTable)); // Allows any public property of the model to be used
-				context.MemberAccessStrategy.Register(typeof(DynamoDbAttribute));
-				context.MemberAccessStrategy.Register(typeof(DynamoDbGlobalSecondaryIndex));
-				context.SetValue("table", table);
+				IEnumerable<string> errors;
+				if (FluidTemplate.TryParse(liquidTemplate, out var template, out errors)) {
+					var context = new TemplateContext();
+					context.MemberAccessStrategy.Register(typeof(DynamoDbTable)); // Allows any public property of the model to be used
+					context.MemberAccessStrategy.Register(typeof(DynamoDbAttribute));
+					context.MemberAccessStrategy.Register(typeof(DynamoDbGlobalSecondaryIndex));
+					context.SetValue("table", table);
 
-				Console.WriteLine(template.Render(context));
-			}
+					Console.WriteLine(template.Render(context));
+				}
+
+				return 0;
+			});
+
+			app.Execute(args);
 		}
 	}
 }
